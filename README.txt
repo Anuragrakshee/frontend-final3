43cb05ffc46e0067796f7825efe501b9417a859c
Anurag Rakshee <anuragrakshee@gmail.com>
2026-01-07 22:43:31 -0800

    new

--- README.txt
+++ README.txt
@@ -1,1 +1,15 @@
-frontend link -- https://frontend-final3.vercel.app/#
+how to run
+
+live links
+frontend link -- https://frontend-final3-17oc.vercel.app/
+register api link -- https://frontend-final3.onrender.com/auth/register
+
+
+
+Tech Stack
+
+Frontend: HTML, CSS, Tailwind, JS
+
+Backend: Node.js, Express
+
+Hosting: Vercel (FE), Render (BE)
--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,4 +1,4 @@
-// Register EV buttons (navbar + anywhere)
+
 const registerBtn = document.getElementById("registerBtn");
 
 if (registerBtn) {
@@ -7,7 +7,7 @@ if (registerBtn) {
   });
 }
 
-// Explore Subsidies button
+
 const exploreBtn = document.getElementById("exploreSubsidies");
 
 if (exploreBtn) {
@@ -19,12 +19,59 @@ if (exploreBtn) {
   });
 }
 
-// Locate Charging button
+
 const locateBtn = document.getElementById("locateCharging");
 
 if (locateBtn) {
   locateBtn.addEventListener("click", (e) => {
     e.preventDefault();
     alert("Charging station map coming soon 🚗⚡");
+  });
+}
+
+
+const form = document.getElementById("registerForm");
+const formSection = document.getElementById("formSection");
+const thankYouCard = document.getElementById("thankyouCard");
+
+if (form) {
+  form.addEventListener("submit", async function (e) {
+    e.preventDefault();
+
+    const userData = {
+      name: document.getElementById("name").value.trim(),
+      email: document.getElementById("email").value.trim(),
+      mobile: document.getElementById("mobile").value.trim(),
+      city: document.getElementById("city").value.trim()
+    };
+
+    try {
+      const response = await fetch("https://frontend-final3.onrender.com/auth/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(userData)
+      });
+
+      const data = await response.json();
+
+      if (!response.ok) {
+        alert(data.message || "Registration failed");
+        return;
+      }
+
+ 
+      if (formSection && thankYouCard) {
+        formSection.style.display = "none";
+        thankYouCard.style.display = "block";
+      }
+
+      console.log("Saved to DB:", data);
+
+    } catch (error) {
+      console.error("API Error:", error);
+      alert("Server error. Please try again later.");
+    }
   });
 }
